7bf30a7dc1e6053acada1e94867e2ce6d258ff21
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-20 13:06:50 +0900

    クリックしたらクラスを付け替えるイベントリスナーを設定

--- app/scripts/script.coffee
+++ app/scripts/script.coffee
@@ -1,3 +1,8 @@
 btn = document.querySelectorAll 'button'
 msg = document.querySelector '.message'
 
+toggleClass = ->
+  msg.classList.toggle 'on'
+
+b.addEventListener 'click', toggleClass for b in btn
+
--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,8 +1,17 @@
 (function() {
-  var btn, msg;
+  var b, btn, msg, toggleClass, _i, _len;
 
   btn = document.querySelectorAll('button');
 
   msg = document.querySelector('.message');
+
+  toggleClass = function() {
+    return msg.classList.toggle('on');
+  };
+
+  for (_i = 0, _len = btn.length; _i < _len; _i++) {
+    b = btn[_i];
+    b.addEventListener('click', toggleClass);
+  }
 
 }).call(this);
